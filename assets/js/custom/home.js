3ae6de9595aa9769ce76ab16ed4fe587f71502d7
Marco Omainska <marcoomainska@icloud.com>
2022-11-06 14:14:32 +0900

    📝 del  publications from landing + animation delay

---
+++ assets/js/custom/home.js
@@ -0,0 +1,12 @@
+/*!
+ * Custom JS file to pause animations on home landing page by Marco Omainska
+ * Copyright 2022 Marco Omainska - marciska.com
+*/
+
+// var content = document.querySelector(".hero-content");
+// // console.log(content)
+// content.classList.add('js-loading');
+// content.addEventListener("load", removeLoadingClass);
+// function removeLoadingClass() {
+//   content.classList.remove('js-loading');
+// }
